9659c245ae307065ae466dfea1d6eea9da77530a
mlnkvcj <milinkovicjovan05@gmail.com>
2024-02-19 12:45:03 +0100

    Kod koji ne radi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
     import Question from "./Question.svelte";
+    
 </script>
 <h1>Kviz</h1>
 
-<Question
-    question = "Kad je bio kosovski boj?"
-    correctAnswer = {1389}
-    incorrectAnswers = {[1989,1835,1456]}/>
+<Question/>
--- src/routes/question.svelte
+++ src/routes/question.svelte
@@ -1,12 +1,12 @@
 <script>
-    export let question;
-    export let correctAnswer;
-    export let incorrectAnswers;
-
+    let question;
+    let correctAnswer;
+    let incorrectAnswers;
+    import { onMount } from "svelte";
     
 
     const shuffle = (array) => array.sort( () => .5 - Math.random() );
-    let possibleAnswers = shuffle([...incorrectAnswers,correctAnswer]);
+    
 
     const handleClick = (answer) =>{
         if(answer == correctAnswer){
@@ -16,11 +16,26 @@
             alert(`Netačno! Tačan odgovor je ${correctAnswer}`)
         }
     }
+   
 
+    const getQuestion = async() => {
+        const res = await fetch("https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple");
+        let data = await res.json();
+        console.log(data);
+        let q;
+        q.question = data.results[0].question;
+        q.correctAnswer = data.results[0].correct_answer;
+        q.incorrectAnswers = data.results[0].incorrect_answers;
+        possibleAnswers = shuffle([...incorrectAnswers,correctAnswer]);
+    }
+    onMount(getQuestion)
+    
 </script>
 
 <div>
     <h3>{@html question}</h3>
+
+
     <div id = "answers">
         {#each possibleAnswers as pa}
             <button on:click= {() => handleClick(pa)}>{pa}</button>
